9abf64e9112578f6ec7f728cae09f2ee1281b0ae
Joshua Huston <joshua.david.huston@gmail.com>
2022-02-02 21:17:24 -0500

    Test Dog API

--- api/serverless_app.py
+++ api/serverless_app.py
@@ -1,14 +1,28 @@
+from http.server import BaseHTTPRequestHandler
+from urllib import parse
 import requests
-response = requests.get("https://api.thedogapi.com/")
-response.text
-# from http.server import BaseHTTPRequestHandler
-# from datetime import datetime
 
-# class handler(BaseHTTPRequestHandler):
+class handler(BaseHTTPRequestHandler):
 
-#   def do_GET(self):
-#     self.send_response(200)
-#     self.send_header('Content-type', 'text/plain')
-#     self.end_headers()
-#     self.wfile.write(str(datetime.now().strftime('%Y-%m-%d %H:%M:%S')).encode())
-#     return
+  def do_GET(self):
+    url_path = self.path
+    url_components = parse.urlsplit(url_path)
+    query_string_list = parse.parse_qsl(url_components.query)
+    dic = dict(query_string_list)
+
+    if "word" in dic:
+        url = 'https://api.thedogapi.com/'
+        r = requests.get(url)
+        data = r.json()
+        
+        message = str(data)        
+    else:
+        message = "Please give me a word to define"
+
+    self.send_response(200)
+    self.send_header('Content-type', 'text/plain')
+    self.end_headers()
+
+    self.wfile.write(message.encode())
+
+    return
